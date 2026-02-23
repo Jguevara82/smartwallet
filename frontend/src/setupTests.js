5d856e0f56143e9338980aa46b40ebb1e4807259
Juan Rozo <juanjose.guevararozo@gmail.com>
2026-02-23 00:55:16 -0500

    fix: add window.matchMedia mock for jsdom test environment

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,18 @@ import { TextEncoder, TextDecoder } from 'util';
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
+
+// Mock window.matchMedia (not implemented in jsdom)
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
